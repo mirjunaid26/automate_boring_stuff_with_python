2d1a0c60335d523221170277b0f43c3da6502279
Mir Junaid <mir.jnd@gmail.com>
2023-03-12 15:59:52 +0100

    Update modules.py

---
+++ modules.py
@@ -0,0 +1,19 @@
+"""
+If you quit from the Python interpreter and
+enter it again, the defintions you have made
+(functions and variables) are lost. Therefore,
+if you want to write a soemwhat longer program,
+you aree better off using a text editor
+to prepare the input for the interpreter and
+running it with that file as input instead.
+This is known as creating a script. As your
+program gets longer, you may want to split it
+into several files for eaisier maintenance.
+You may also want to use a handy function
+that you've written in several programs
+without coying its definition into
+each program.
+
+To
+
+"""
