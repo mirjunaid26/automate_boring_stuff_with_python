e9ac0fde426e8522050b26c510c0a8a821cfba30
Mir Junaid <mir.jnd@gmail.com>
2023-03-12 16:24:17 +0100

    modules-branch

--- modules.py
+++ modules.py
@@ -29,3 +29,19 @@ For instance, use your favourite text editor to create a file called module.py
 in the current directory with the following contents:
 
 """
+
+# Fibonacci numbers module
+def fib(n): # write Fibonacci series upto n
+    a, b = 0, 1
+    while a < n:
+        print (a, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n):  # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b =  b, a+b
+    return result
---
+++ test-modules.py
@@ -0,0 +1,4 @@
+import modules
+modules.fib(10)
+modules.fib2(100)
+modules.__name__
