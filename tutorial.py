90e40803db1f5fcf467605f2096ca5e90d7be9fc
Mir Junaid <mir.jnd@gmail.com>
2023-03-12 12:23:24 +0100

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -95,3 +95,21 @@ for i in range(len(a)):
 
 x = sum(range(4))
 print(x)
+
+# Break and continue Statements, and else Clauses on Loops
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n)
+            break
+        else:
+            # loops fell through without finding a factor
+            print(n, 'is a prime number')
+
+
+for num in range(10, 15):
+    if num % 2 == 0:
+        print("Found an even number", num)
+        continue
+    print("Found an odd number", num)
