0f55f21a651d143fd6f10a3db74afda3e69a7fc6
Mir Junaid <mir.jnd@gmail.com>
2023-03-14 08:17:58 +0100

    Update exception-handling.py

--- exception-handling.py
+++ exception-handling.py
@@ -1,1 +1,27 @@
 # Exception Handling
+
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops! That was no valid number. Try again.")
+
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
